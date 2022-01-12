78533631b3ef9ba3f8bec57e391addcade1731ef
lzlocki <lzlocki@poczta.fm>
2022-01-12 16:47:04 +0100

    Add Resource and Role models

---
+++ BugTracer.Data/Models/Resource.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTracer.Data.Models
+{
+    public class Resource
+    {
+        public int ResourceId { get; set; }
+
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+        [MaxLength(20)]
+        public string LastName { get; set; }
+        [MaxLength(64)]
+        public string Email { get; set; }
+
+        public ResourceRole Role { get; set; }
+
+    }
+}
---
+++ BugTracer.Data/Models/ResourceRole.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTracer.Data.Models
+{
+    public class ResourceRole
+    {
+        public int RoleId { get; set; }
+
+        [MaxLength(20)]
+        public string Role { get; set; }
+
+        public int ResourceId { get; set; }
+        public Resource Resource { get; set; }
+    }
+}
