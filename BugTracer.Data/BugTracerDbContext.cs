8474e4edf264cb5b9dde11c700b56b4d296be76e
lzlocki <lzlocki@poczta.fm>
2022-01-18 21:11:40 +0100

    Set and configure sql database

---
+++ BugTracer.Data/BugTracerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BugTracer.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTracer.Data
+{
+    public class BugTracerDbContext : DbContext
+    {
+        public BugTracerDbContext(DbContextOptions<BugTracerDbContext> options) : base(options) { }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<ResourceRole>  ResourceRoles { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<TicketPriority> TicketPriorities { get; set; }
+        public DbSet<TicketStatus> TicketStatuses { get; set; }
+    }
+}
