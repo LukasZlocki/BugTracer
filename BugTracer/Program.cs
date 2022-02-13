2c1ffd0522f5e0aed13d21cf5cdbd1b95b08e0be
lzlocki <lzlocki@poczta.fm>
2022-02-13 13:39:04 +0100

    Add project service

--- BugTracer/Program.cs
+++ BugTracer/Program.cs
@@ -1,4 +1,5 @@
 using BugTracer.Data;
+using BugTracer.Services.Project_Service;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,7 @@ builder.Services.AddDbContext<BugTracerDbContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IProjectService, ProjectService>();
 
 var app = builder.Build();
 
