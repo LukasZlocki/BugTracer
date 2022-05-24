ee5ddfbd32e085825233f2378ab20ad7d6f683d9
lzlocki <lzlocki@poczta.fm>
2022-05-24 21:12:50 +0200

    Add priotity service and its interface

---
+++ BugTracer.Services/Priority_Service/IPriorityService.cs
@@ -0,0 +1,19 @@
+﻿using BugTracer.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracer.Services.Priority_Service
+{
+    public interface IPriorityService
+    {
+        // READ
+        public List<TicketPriority> GetAllPriorities();
+
+        // READ
+        public TicketPriority GetPriorityById(int id);
+
+    }
+}
---
+++ BugTracer.Services/Priority_Service/PriorityService.cs
@@ -0,0 +1,43 @@
+﻿using BugTracer.Data;
+using BugTracer.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracer.Services.Priority_Service
+{
+    public class PriorityService : IPriorityService
+    {
+        private readonly BugTracerDbContext _db;
+
+        public PriorityService(BugTracerDbContext db)
+        {
+            _db = db;    
+        }
+        
+        // READ
+        /// <summary>
+        /// Returns priority list 
+        /// </summary>
+        /// <returns>List<TicketPriority></returns>
+        public List<TicketPriority> GetAllPriorities()
+        {
+            var service = _db.TicketPriorities.ToList();
+            return service;
+        }
+
+        // READ
+        /// <summary>
+        /// Returns priority object by its primary key
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public TicketPriority GetPriorityById(int id)
+        {
+            var service = _db.TicketPriorities.Find(id);
+            return service;
+        }
+    }
+}
