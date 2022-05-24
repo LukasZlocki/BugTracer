bd2b9d9c2ac72425d0c813ea659163eb690c245e
lzlocki <lzlocki@poczta.fm>
2022-05-24 22:11:19 +0200

    Add status service and its interface

---
+++ BugTracer.Services/Status_Service/IStatusService.cs
@@ -0,0 +1,17 @@
+﻿using BugTracer.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracer.Services.Status_Service
+{
+    public interface IStatusService
+    {
+
+        // READ
+        public TicketStatus GetStatusById(int id);
+
+    }
+}
