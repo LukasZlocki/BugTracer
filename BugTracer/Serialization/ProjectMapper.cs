1feb3491c35224f7f93af109d81144ad2c632e7e
lzlocki <lzlocki@poczta.fm>
2022-06-25 14:58:14 +0200

    Remove unneeded methods

--- BugTracer/Serialization/ProjectMapper.cs
+++ BugTracer/Serialization/ProjectMapper.cs
@@ -19,19 +19,6 @@ namespace BugTracer.Api.Serialization
             };
         }
 
-        public static Project SerializeProjectReadDtoModelToProjectModel(ProjectReadDto projectReadDto)
-        {
-            return new Project
-            {
-                Id = projectReadDto.Id,
-                Name = projectReadDto.Name,
-                Description = projectReadDto.Description,
-                CreatedOn = projectReadDto.CreatedOn,
-                CreatedBy = projectReadDto.CreatedBy,
-                ClosedOn = projectReadDto.ClosedOn,
-                ClosedBy = projectReadDto.ClosedBy,
-            };
-        }
 
         public static List<ProjectBasicReadDto> SerializeProjectModelListToProjectReadDtoList(IEnumerable<Project> projects)
         {
@@ -44,20 +31,6 @@ namespace BugTracer.Api.Serialization
                 CreatedBy = project.CreatedBy,
                 ClosedOn= project.ClosedOn,
                 ClosedBy= project.ClosedBy,
-            }).ToList();
-        }
-
-        public static List<Project> SerializeProjectReadDtoListToProjectList(IEnumerable<ProjectReadDto> projectsReadDto)
-        {
-            return projectsReadDto.Select(projectsDto => new Project 
-            {
-                Id = projectsDto.Id,
-                Name = projectsDto.Name,
-                Description = projectsDto.Description,
-                CreatedOn = projectsDto.CreatedOn,
-                CreatedBy = projectsDto.CreatedBy,
-                ClosedOn = projectsDto.ClosedOn,
-                ClosedBy = projectsDto.ClosedBy,     
             }).ToList();
         }
 
